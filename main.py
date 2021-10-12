13c5ba8afe4ba5542e9ade3931d6b49dc791344e
AlecsandraMuha <alecsandramuha@yahoo.com>
2021-10-12 15:01:58 +0300

    commit

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 def read_list():
     '''
-    Functie pentru citirea listei
+    functia citeste lista
     :return: lista citita
     '''
     lst = []
-    lst_str = input('Dați numerele separate prin spațiu: ')
+    lst_str = input('Dați numerele : ')
     lst_str_split = lst_str.split(' ')
     for num_str in lst_str_split:
         lst.append(int(num_str))
     return lst
-fwegweg
